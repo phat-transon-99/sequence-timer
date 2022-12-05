fb9e7725afdd162b11c79adaf2e2717f0c8a0d37
phat-transon-99 <118055078+phat-transon-99@users.noreply.github.com>
2022-12-05 14:44:51 +0700

    Add BottomCard component

---
+++ src/screens/HomeScreen/components/BottomCard.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import { whiteColor } from '../../../styles/colors';
+
+type CardProps = {
+  children?: React.ReactNode
+};
+
+const styles = StyleSheet.create({
+  card: {
+    backgroundColor: whiteColor,
+    borderTopLeftRadius: 40,
+    borderTopRightRadius: 40,
+    flex: 2.5,
+    padding: 20,
+  },
+});
+
+function BottomCard({ children }: CardProps): JSX.Element {
+  return (
+    <View style={styles.card}>{ children }</View>
+  );
+}
+
+export default BottomCard;
