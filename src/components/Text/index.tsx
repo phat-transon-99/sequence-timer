616e92a5efb49cba4d088124ea8950ff3556a909
phat-transon-99 <118055078+phat-transon-99@users.noreply.github.com>
2022-12-06 14:07:56 +0700

    Add bold text

--- src/components/Text/index.tsx
+++ src/components/Text/index.tsx
@@ -9,12 +9,19 @@ const styles = StyleSheet.create({
   text: {
     fontFamily: 'Poppins_400Regular',
   },
+  textBold: {
+    fontFamily: 'Poppins_500Medium',
+  },
 });
 
-function CustomFontText({ children }: CustomFontTextProps): JSX.Element {
+export function CustomFontText({ children }: CustomFontTextProps): JSX.Element {
   return (
     <Text style={styles.text}>{ children }</Text>
   );
 }
 
-export default CustomFontText;
+export function CustomFontTextBold({ children }: CustomFontTextProps): JSX.Element {
+  return (
+    <Text style={styles.textBold}>{ children }</Text>
+  );
+}
