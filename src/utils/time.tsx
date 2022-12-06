e1ee57f0865060a3b2cc3043f538511b31674357
phat-transon-99 <118055078+phat-transon-99@users.noreply.github.com>
2022-12-06 09:51:46 +0700

    Add time format utils

---
+++ src/utils/time.tsx
@@ -0,0 +1,20 @@
+function prependZero(num: number): string {
+  if (num < 10) {
+    return `0${num}`;
+  }
+  return `${num}`;
+}
+
+export default function formatTimeStandard(timeInSeconds: number): string {
+  // Get the hours
+  const hours = Math.floor(timeInSeconds / 3600);
+
+  // Get the minutes
+  const minutes = Math.floor((timeInSeconds - hours * 3600) / 60);
+
+  // Get the seconds
+  const seconds = timeInSeconds - hours * 3600 - minutes * 60;
+
+  // Format the time and return
+  return `${prependZero(hours)}:${prependZero(minutes)}:${prependZero(seconds)}`;
+}
