51fa645fcffd6121bfa6662ea82fc055a2ee4ca3
phat-transon-99 <118055078+phat-transon-99@users.noreply.github.com>
2022-12-06 10:45:01 +0700

    Add use fonts to application

--- App.tsx
+++ App.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import useCombineBooleanHooks from './src/hooks/combine';
+import useLoadFont from './src/hooks/fonts';
 import HomeScreen from './src/screens/HomeScreen';
 
 export default function App() {
+  const isAppReady = useCombineBooleanHooks(useLoadFont);
+
   return (
-    <HomeScreen />
+    isAppReady && <HomeScreen />
   );
 }
