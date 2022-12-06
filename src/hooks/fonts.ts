ea33599bdb11e99770ac691b9ea2b771f35b4dbd
phat-transon-99 <118055078+phat-transon-99@users.noreply.github.com>
2022-12-06 14:08:09 +0700

    Load bold font

--- src/hooks/fonts.ts
+++ src/hooks/fonts.ts
@@ -1,8 +1,8 @@
-import { useFonts, Poppins_400Regular } from '@expo-google-fonts/poppins';
+import { useFonts, Poppins_400Regular, Poppins_500Medium } from '@expo-google-fonts/poppins';
 import BooleanHook from './types';
 
 const useLoadFont: BooleanHook = () => {
-  const [fontLoaded] = useFonts({ Poppins_400Regular });
+  const [fontLoaded] = useFonts({ Poppins_400Regular, Poppins_500Medium });
   return fontLoaded;
 };
 
