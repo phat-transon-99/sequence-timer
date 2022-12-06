82ebc88208837c1f00a77058cdb33868589a142f
phat-transon-99 <118055078+phat-transon-99@users.noreply.github.com>
2022-12-06 09:51:17 +0700

    Add test card

--- src/screens/HomeScreen/index.tsx
+++ src/screens/HomeScreen/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { View, StyleSheet } from 'react-native';
 import { primaryColor } from '../../styles/colors';
 import BottomCard from './components/BottomCard';
+import TimerCard from './components/TimerCard';
 import TopTimeDisplay from './components/TopTimeDisplay';
 
 const styles = StyleSheet.create({
@@ -15,7 +16,9 @@ function HomeScreen(): JSX.Element {
   return (
     <View style={styles.screen}>
       <TopTimeDisplay />
-      <BottomCard />
+      <BottomCard>
+        <TimerCard name="Drawing" icon="🌋" timeInSeconds={1600} color="#00C0FF" />
+      </BottomCard>
     </View>
   );
 }
