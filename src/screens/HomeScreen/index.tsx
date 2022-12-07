1a823ce51c33fed4a5223de2a438a9be9990d56d
phat-transon-99 <118055078+phat-transon-99@users.noreply.github.com>
2022-12-07 10:59:21 +0700

    Test header

--- src/screens/HomeScreen/index.tsx
+++ src/screens/HomeScreen/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { View, StyleSheet } from 'react-native';
 import { primaryColor } from '../../styles/colors';
 import BottomCard from './components/BottomCard';
+import Header from './components/Header';
 import TimerCardList, { ExtendedTimerCardProps } from './components/TimerCardList';
 import TopTimeDisplay from './components/TopTimeDisplay';
 
@@ -17,13 +18,13 @@ const items: ExtendedTimerCardProps[] = [
     id: 1, name: 'Drawing', icon: '🌋', timeInSeconds: 1600, color: '#F77F00',
   },
   {
-    id: 2, name: 'Drawing', icon: '🌋', timeInSeconds: 1600, color: '#F77F00',
+    id: 2, name: 'Drawing', icon: '🌋', timeInSeconds: 1600, color: '#2a9d8f',
   },
   {
-    id: 3, name: 'Drawing', icon: '🌋', timeInSeconds: 1600, color: '#F77F00',
+    id: 3, name: 'Drawing', icon: '🌋', timeInSeconds: 1600, color: '#d62828',
   },
   {
-    id: 4, name: 'Drawing', icon: '🌋', timeInSeconds: 1600, color: '#F77F00',
+    id: 4, name: 'Drawing', icon: '🌋', timeInSeconds: 1600, color: '#7209b7',
   },
 ];
 
@@ -32,6 +33,7 @@ function HomeScreen(): JSX.Element {
     <View style={styles.screen}>
       <TopTimeDisplay />
       <BottomCard>
+        <Header onAddClick={() => {}} />
         <TimerCardList timers={items} />
       </BottomCard>
     </View>
