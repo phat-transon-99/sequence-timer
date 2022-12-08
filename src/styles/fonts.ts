9b471dea44e503af87ef2b41a557f9b37a869733
phat-transon-99 <118055078+phat-transon-99@users.noreply.github.com>
2022-12-08 09:21:24 +0700

    Add font family

--- src/styles/fonts.ts
+++ src/styles/fonts.ts
@@ -1,4 +1,10 @@
+// [] FONT SIZE
 export const FONTSIZE_SMALL = 16;
 export const FONTSIZE_MEDIUM = 20;
 export const FONTSIZE_LARGE = 24;
 export const FONTSIZE_XLARGE = 40;
+
+// [] FONT FAMILY
+export const FONTFAMILY_REGULAR = 'Poppins_400Regular';
+export const FONTFAMLIY_MEDIUM = 'Poppins_500Medium';
+export const FONTFAMILY_BOLD = 'Poppins_600SemiBold';
