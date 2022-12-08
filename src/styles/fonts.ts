63bdad62809677a6596e7a9375edbb978ffbb75b
phat-transon-99 <118055078+phat-transon-99@users.noreply.github.com>
2022-12-08 10:00:01 +0700

    Add font weights

--- src/styles/fonts.ts
+++ src/styles/fonts.ts
@@ -1,4 +1,5 @@
 // [] FONT SIZE
+export const FONTSIZE_XSMALL = 12;
 export const FONTSIZE_SMALL = 16;
 export const FONTSIZE_MEDIUM = 20;
 export const FONTSIZE_LARGE = 24;
@@ -7,4 +8,9 @@ export const FONTSIZE_XLARGE = 40;
 // [] FONT FAMILY
 export const FONTFAMILY_REGULAR = 'Poppins_400Regular';
 export const FONTFAMLIY_MEDIUM = 'Poppins_500Medium';
-export const FONTFAMILY_BOLD = 'Poppins_600SemiBold';
+export const FONTFAMILY_SEMIBOLD = 'Poppins_600SemiBold';
+
+// [] FONT WEIGHT
+export const FONTWEIGHT_REGULAR = '400';
+export const FONTWEIGHT_MEDIUM = '500';
+export const FONTWEIGHT_SEMIBOLD = '600';
