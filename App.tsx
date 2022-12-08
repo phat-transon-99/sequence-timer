1bdda884e91a392ef81af3cf8b34c37f57ea89e0
phat-transon-99 <118055078+phat-transon-99@users.noreply.github.com>
2022-12-08 10:24:09 +0700

    Test HomeScreen

--- App.tsx
+++ App.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import useCombineBooleanHooks from './src/hooks/combine';
 import useLoadFont from './src/hooks/fonts';
-import CreateTimerScreen from './src/screens/CreateTimerScreen';
+import HomeScreen from './src/screens/HomeScreen';
 
 export default function App() {
   // @ts-ignore Disable ts error on this line
   const isAppReady = useCombineBooleanHooks(useLoadFont);
 
   return (
-    isAppReady && <CreateTimerScreen />
+    isAppReady && <HomeScreen />
   );
 }
