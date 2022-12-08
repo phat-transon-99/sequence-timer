d34d3ea6d2b25089cb1801ee3bc7d4717d831119
phat-transon-99 <118055078+phat-transon-99@users.noreply.github.com>
2022-12-08 14:31:44 +0700

    Test textbox

--- App.tsx
+++ App.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import useCombineBooleanHooks from './src/hooks/combine';
 import useLoadFont from './src/hooks/fonts';
-import HomeScreen from './src/screens/HomeScreen';
+import CreateTimerScreen from './src/screens/CreateTimerScreen';
 
 export default function App() {
   // @ts-ignore Disable ts error on this line
   const isAppReady = useCombineBooleanHooks(useLoadFont);
 
   return (
-    isAppReady && <HomeScreen />
+    isAppReady && <CreateTimerScreen />
   );
 }
--- src/screens/CreateTimerScreen/index.tsx
+++ src/screens/CreateTimerScreen/index.tsx
@@ -2,13 +2,16 @@ import React from 'react';
 import { View } from 'react-native';
 import DefaultView from '../../components/DefaultView';
 import Header from '../../components/Header';
+import TextBox from '../../components/TextBox';
 import styles from './index.style';
+import { WHITE_COLOR } from '../../styles/colors';
 
 export default function CreateTimerScreen(): JSX.Element {
   return (
-    <DefaultView>
+    <DefaultView color={WHITE_COLOR}>
       <View style={styles.container}>
         <Header>Select time</Header>
+        <TextBox placeholder="Enter time" />
       </View>
     </DefaultView>
   );
