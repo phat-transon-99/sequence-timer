ea1295e55c1ba66df86caab68d300f45f9e81e75
phat-transon-99 <118055078+phat-transon-99@users.noreply.github.com>
2022-12-08 14:55:17 +0700

    Test

--- src/screens/CreateTimerScreen/index.tsx
+++ src/screens/CreateTimerScreen/index.tsx
@@ -5,13 +5,17 @@ import Header from '../../components/Header';
 import TextBox from '../../components/TextBox';
 import styles from './index.style';
 import { WHITE_COLOR } from '../../styles/colors';
+import ColorPicker from '../../components/ColorPicker';
 
 export default function CreateTimerScreen(): JSX.Element {
   return (
     <DefaultView color={WHITE_COLOR}>
       <View style={styles.container}>
-        <Header>Select time</Header>
+        <Header>Select name</Header>
         <TextBox placeholder="Enter time" />
+
+        <Header>Select color</Header>
+        <ColorPicker />
       </View>
     </DefaultView>
   );
