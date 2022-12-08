09071efb8dc2233dbe173aee088f036f75325128
phat-transon-99 <118055078+phat-transon-99@users.noreply.github.com>
2022-12-08 09:20:41 +0700

    Add text style

---
+++ src/components/Text/index.style.ts
@@ -0,0 +1,11 @@
+import { StyleSheet } from 'react-native';
+import { FONTFAMILY_REGULAR, FONTFAMLIY_MEDIUM } from '../../styles/fonts';
+
+export default StyleSheet.create({
+  text: {
+    fontFamily: FONTFAMILY_REGULAR,
+  },
+  textBold: {
+    fontFamily: FONTFAMLIY_MEDIUM,
+  },
+});
