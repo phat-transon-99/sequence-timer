8a1d98957da06619988db65ba4c1b6f53ac58e74
phat-transon-99 <118055078+phat-transon-99@users.noreply.github.com>
2022-12-08 10:24:21 +0700

    Add background color

--- src/components/DefaultView/index.style.ts
+++ src/components/DefaultView/index.style.ts
@@ -1,9 +1,11 @@
 import { StyleSheet } from 'react-native';
+import { PRIMARY_COLOR } from '../../styles/colors';
 import { PADDING_LARGE } from '../../styles/dims';
 
 export default StyleSheet.create({
   container: {
     flex: 1,
     paddingTop: PADDING_LARGE,
+    backgroundColor: PRIMARY_COLOR,
   },
 });
