9f59b83373c8882bf52546e81237702f02951e43
phat-transon-99 <118055078+phat-transon-99@users.noreply.github.com>
2022-12-08 09:59:53 +0700

    Add padding and margin dims

---
+++ src/styles/dims.ts
@@ -0,0 +1,9 @@
+// [] PADDING
+export const PADDING_SMALL = 20;
+export const PADDING_MEDIUM = 30;
+export const PADDING_LARGE = 40;
+
+// [] MARGIN
+export const MARGIN_SMALL = 10;
+export const MARGIN_MEDIUM = 20;
+export const MARGIN_LARGE = 30;
