cfe79d5268be63734300aa7b701f3d66def34dc0
phat-transon-99 <118055078+phat-transon-99@users.noreply.github.com>
2022-12-08 09:20:50 +0700

    Add text style

--- src/components/Text/index.tsx
+++ src/components/Text/index.tsx
@@ -1,18 +1,10 @@
 import React from 'react';
-import { Text, StyleSheet } from 'react-native';
+import { Text } from 'react-native';
+import styles from './index.style';
 
 type CustomFontTextProps = {
   children: React.ReactNode
 };
-
-const styles = StyleSheet.create({
-  text: {
-    fontFamily: 'Poppins_400Regular',
-  },
-  textBold: {
-    fontFamily: 'Poppins_500Medium',
-  },
-});
 
 export function CustomFontText({ children }: CustomFontTextProps): JSX.Element {
   return (
