afbfd5e6e5439fae392d3a7c34c4e1720b6296c6
phat-transon-99 <118055078+phat-transon-99@users.noreply.github.com>
2022-12-08 10:07:12 +0700

    Add border radius

--- src/styles/dims.ts
+++ src/styles/dims.ts
@@ -7,3 +7,10 @@ export const PADDING_LARGE = 40;
 export const MARGIN_SMALL = 10;
 export const MARGIN_MEDIUM = 20;
 export const MARGIN_LARGE = 30;
+
+// [] BORDER RADIUS
+export const BORDERRADIUS_XSMALL = 15;
+export const BORDERRADIUS_SMALL = 20;
+export const BORDERRADIUS_MEDIUM = 30;
+export const BORDERRADIUS_LARGE = 40;
+export const BORDERRADIUS_ULARGE = 100;
