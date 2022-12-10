3c60dbcd1d887fae21deea063142265e5f3e261f
phat-transon-99 <118055078+phat-transon-99@users.noreply.github.com>
2022-12-10 08:39:39 +0700

    Add transparent color

--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -3,3 +3,4 @@ export const WHITE_COLOR = '#ffffff';
 export const BLACK_COLOR = '#000000';
 export const GRAY_COLOR = '#A1A1A1';
 export const TRANSPARENT_COLOR = 'transparent';
+export const TRANSPARENT_BLACK = 'rgba(0, 0, 0, 0.5)';
