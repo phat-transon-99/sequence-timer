657d304f0b89bdae0f64c785f3a34404779f566d
phat-transon-99 <118055078+phat-transon-99@users.noreply.github.com>
2022-12-10 14:12:26 +0700

    Add color

--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -1,6 +1,7 @@
 export const PRIMARY_COLOR = '#4361EE';
-export const WHITE_COLOR = '#ffffff';
+export const WHITE_COLOR = '#FFFFFF';
 export const BLACK_COLOR = '#000000';
 export const GRAY_COLOR = '#A1A1A1';
+export const RED_COLOR = '#E63946';
 export const TRANSPARENT_COLOR = 'transparent';
 export const TRANSPARENT_GRAY_COLOR = 'rgba(161, 161, 161, 0.5)';
