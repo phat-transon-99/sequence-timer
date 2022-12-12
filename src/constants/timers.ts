fb6767bc5bcb63a68c3b26c4de986590460c1e54
phat-transon-99 <118055078+phat-transon-99@users.noreply.github.com>
2022-12-12 15:50:40 +0700

    Extract constant timers

---
+++ src/constants/timers.ts
@@ -0,0 +1,18 @@
+import Timer from '../models/Timer';
+
+const TIMERS: Timer[] = [
+  {
+    id: 1, name: 'Drawing', duration: 1600, color: '#F77F00',
+  },
+  {
+    id: 2, name: 'Drawing', duration: 1600, color: '#2a9d8f',
+  },
+  {
+    id: 3, name: 'Drawing', duration: 1600, color: '#d62828',
+  },
+  {
+    id: 4, name: 'Drawing', duration: 1600, color: '#7209b7',
+  },
+];
+
+export default TIMERS;
--- src/screens/HomeScreen/index.tsx
+++ src/screens/HomeScreen/index.tsx
@@ -1,28 +1,13 @@
 import React from 'react';
 import { View } from 'react-native';
 import DefaultView from '../../components/SafeAreaView';
-import Timer from '../../models/Timer';
 import Header from './components/Header';
 import TimerCardList from './components/TimerCardList';
 import TopTimeDisplay from './components/TopTimeDisplay';
 import styles from './index.style';
 import { PRIMARY_COLOR } from '../../styles/colors';
 import CustomBottomSheet from '../../components/CustomBottomSheet';
-
-const items: Timer[] = [
-  {
-    id: 1, name: 'Drawing', duration: 1600, color: '#F77F00',
-  },
-  {
-    id: 2, name: 'Drawing', duration: 1600, color: '#2a9d8f',
-  },
-  {
-    id: 3, name: 'Drawing', duration: 1600, color: '#d62828',
-  },
-  {
-    id: 4, name: 'Drawing', duration: 1600, color: '#7209b7',
-  },
-];
+import TIMERS from '../../constants/timers';
 
 function HomeScreen(): JSX.Element {
   return (
@@ -31,7 +16,7 @@ function HomeScreen(): JSX.Element {
         <TopTimeDisplay />
         <CustomBottomSheet>
           <Header onAddClick={() => {}} />
-          <TimerCardList timers={items} />
+          <TimerCardList timers={TIMERS} />
         </CustomBottomSheet>
       </View>
     </DefaultView>
