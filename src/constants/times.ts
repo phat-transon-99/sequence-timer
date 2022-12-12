38e80b2db44237124f6bc26fd044a327c7fb5cf2
phat-transon-99 <118055078+phat-transon-99@users.noreply.github.com>
2022-12-12 14:02:18 +0700

    Generate hours, minutes and seconds

--- src/constants/times.ts
+++ src/constants/times.ts
@@ -1,58 +1,61 @@
 import { DropdownItem } from '../components/Dropdown/props';
 
-export const HOURS: DropdownItem[] = [
-  {
-    title: '0 hour',
-    value: '0_hour',
-  },
-  {
-    title: '1 hour',
-    value: '1_hour',
-  },
-  {
-    title: '2 hours',
-    value: '2_hours',
-  },
-  {
-    title: '3 hours',
-    value: '3_hours',
-  },
-];
+export const HOURS: DropdownItem[] = (function generateHours(): DropdownItem[] {
+  const hours: DropdownItem[] = [];
 
-export const MINUTES: DropdownItem[] = [
-  {
-    title: '0 minute',
-    value: '0_minute',
-  },
-  {
-    title: '1 minute',
-    value: '1_minute',
-  },
-  {
-    title: '2 minutes',
-    value: '2_minutes',
-  },
-  {
-    title: '3 minutes',
-    value: '3_minutes',
-  },
-];
+  for (let i = 0; i !== 61; i += 1) {
+    if (i === 0 || i === 1) {
+      hours.push({
+        title: `${i} hour`,
+        value: `${i}_hour`,
+      });
+    } else {
+      hours.push({
+        title: `${i} hours`,
+        value: `${i}_hours`,
+      });
+    }
+  }
 
-export const SECONDS: DropdownItem[] = [
-  {
-    title: '0 second',
-    value: '0_second',
-  },
-  {
-    title: '1 second',
-    value: '1_second',
-  },
-  {
-    title: '2 seconds',
-    value: '2_seconds',
-  },
-  {
-    title: '3 seconds',
-    value: '3_seconds',
-  },
-];
+  return hours;
+}());
+
+export const MINUTES: DropdownItem[] = (function generateMinutes(): DropdownItem[] {
+  const minutes: DropdownItem[] = [];
+
+  for (let i = 0; i !== 61; i += 1) {
+    if (i === 0 || i === 1) {
+      minutes.push({
+        title: `${i} minute`,
+        value: `${i}_minute`,
+      });
+    } else {
+      minutes.push({
+        title: `${i} minutes`,
+        value: `${i}_minutes`,
+      });
+    }
+  }
+
+  return minutes;
+}());
+
+export const SECONDS: DropdownItem[] = (function generateSeconds(): DropdownItem[] {
+  const seconds: DropdownItem[] = [];
+
+  for (let i = 0; i !== 61; i += 1) {
+    if (i === 0 || i === 1) {
+      seconds.push({
+        title: `${i} second`,
+        value: `${i}_second`,
+      });
+    } else {
+      seconds.push({
+        title: `${i} seconds`,
+        value: `${i}_seconds`,
+      });
+    }
+  }
+
+  return seconds;
+}());
