630e05c518a556c09514d684001ea52761e9e2ea
phat-transon-99 <118055078+phat-transon-99@users.noreply.github.com>
2022-12-12 14:18:51 +0700

    Control text

--- src/components/TextBox/index.tsx
+++ src/components/TextBox/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import { TextInput } from 'react-native';
 import styles from './index.style';
 import { GRAY_COLOR } from '../../styles/colors';
@@ -9,12 +9,20 @@ type Props = {
 };
 
 export default function TextBox({ placeholder, onChangeText }: Props): JSX.Element {
+  const [text, setText] = useState<string>();
+
+  const onChangeTextHandler = useCallback((textValue: string) => {
+    setText(textValue);
+    onChangeText?.(textValue);
+  }, []);
+
   return (
     <TextInput
+      value={text}
       selectionColor={GRAY_COLOR}
       style={styles.textBox}
       placeholder={placeholder}
-      onChangeText={onChangeText}
+      onChangeText={onChangeTextHandler}
     />
   );
 }
