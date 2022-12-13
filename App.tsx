b7a257e226e9c12664cd7bf7201ce8d9300ea397
phat-transon-99 <118055078+phat-transon-99@users.noreply.github.com>
2022-12-13 13:30:29 +0700

    Add React Navigation

--- App.tsx
+++ App.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import useCombineBooleanHooks from './src/hooks/combine';
 import useLoadFont from './src/hooks/fonts';
 import HomeScreen from './src/screens/HomeScreen';
+import CreateTimerScreen from './src/screens/CreateTimerScreen';
 import store from './src/store';
+
+// Create stack navigation
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   // @ts-ignore Disable ts error on this line
@@ -13,7 +19,12 @@ export default function App() {
     isAppReady
     && (
     <Provider store={store}>
-      <HomeScreen />
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="CreateTimer" component={CreateTimerScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </Provider>
     )
   );
