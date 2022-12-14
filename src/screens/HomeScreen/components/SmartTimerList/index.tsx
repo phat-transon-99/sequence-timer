3991f7bbc66bf9957c724dbdef6a78b48819ecbe
phat-transon-99 <118055078+phat-transon-99@users.noreply.github.com>
2022-12-14 15:13:47 +0700

    Add smart timer list

---
+++ src/screens/HomeScreen/components/SmartTimerList/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Text } from 'react-native';
+import selectTimers from '../../../../features/MangeTimer/selector';
+import { useAppSelector } from '../../../../hooks/redux';
+import TimerCardList from '../TimerCardList';
+import Timer from '../../../../models/Timer';
+
+function renderTimerList(loading: boolean, error: string | boolean, timers: Timer[]): JSX.Element {
+  if (loading) {
+    return <Text>Is Loading</Text>;
+  } if (error) {
+    return <Text>Error encountered</Text>;
+  }
+  return <TimerCardList timers={timers} />;
+}
+
+export default function SmartTimerList(): JSX.Element {
+  // Get properties from state
+  const { loading, error, body } = useAppSelector(selectTimers);
+  return renderTimerList(loading, error, body || []);
+}
