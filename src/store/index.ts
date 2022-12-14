662461c5559e09dc2e8422b46d2933fcdcb975f2
phat-transon-99 <118055078+phat-transon-99@users.noreply.github.com>
2022-12-14 15:14:10 +0700

    Alter export

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,7 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
 import manageTimerReducer from '../features/MangeTimer/manageTimerSlice';
 
-const store = configureStore({
+export const store = configureStore({
   reducer: {
     timers: manageTimerReducer,
   },
