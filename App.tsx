ce9510ac3517dd39b14918c1150ebc907dacfc9e
phat-transon-99 <118055078+phat-transon-99@users.noreply.github.com>
2022-12-14 15:12:07 +0700

    Change redux

--- App.tsx
+++ App.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import { Provider } from 'react-redux';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Provider } from 'react-redux';
 import useCombineBooleanHooks from './src/hooks/combine';
 import useLoadFont from './src/hooks/fonts';
 import HomeScreen from './src/screens/HomeScreen';
 import CreateTimerScreen from './src/screens/CreateTimerScreen';
-import store from './src/store';
 import useDatabase from './src/hooks/database';
+import { store } from './src/store';
 
 // Create stack navigation
 const Stack = createNativeStackNavigator();
@@ -21,15 +21,15 @@ export default function App() {
 
   return (
     isAppReady
-    && (
-    <Provider store={store}>
-      <NavigationContainer>
-        <Stack.Navigator screenOptions={{ headerShown: false }}>
-          <Stack.Screen name="Home" component={HomeScreen} />
-          <Stack.Screen name="CreateTimer" component={CreateTimerScreen} />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </Provider>
-    )
+      ? (
+        <Provider store={store}>
+          <NavigationContainer>
+            <Stack.Navigator screenOptions={{ headerShown: false }}>
+              <Stack.Screen name="Home" component={HomeScreen} />
+              <Stack.Screen name="CreateTimer" component={CreateTimerScreen} />
+            </Stack.Navigator>
+          </NavigationContainer>
+        </Provider>
+      ) : null
   );
 }
