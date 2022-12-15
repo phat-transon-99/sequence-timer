1acd594ea5708faf285040a19572cf91f03e1081
phat-transon-99 <118055078+phat-transon-99@users.noreply.github.com>
2022-12-15 13:59:56 +0700

    Add UpdateTimerScreen

---
+++ src/screens/UpdateTimerScreen/index.style.ts
@@ -0,0 +1,32 @@
+import { StyleSheet } from 'react-native';
+import {
+  MARGIN_MEDIUM, MARGIN_SMALL, PADDING_SMALL,
+} from '../../styles/dims';
+import {
+  FONTFAMILY_SEMIBOLD, FONTSIZE_LARGE, FONTWEIGHT_SEMIBOLD,
+} from '../../styles/fonts';
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: PADDING_SMALL,
+    paddingVertical: PADDING_SMALL,
+  },
+  spaced: {
+    marginBottom: MARGIN_MEDIUM,
+  },
+  buttonSpaced: {
+    marginBottom: MARGIN_SMALL,
+  },
+  title: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom: MARGIN_SMALL,
+    marginTop: MARGIN_MEDIUM,
+  },
+  titleText: {
+    fontFamily: FONTFAMILY_SEMIBOLD,
+    fontSize: FONTSIZE_LARGE,
+    fontWeight: FONTWEIGHT_SEMIBOLD,
+  },
+});
---
+++ src/screens/UpdateTimerScreen/index.tsx
@@ -0,0 +1,85 @@
+import React, { useCallback, useRef } from 'react';
+import { ScrollView, View, Text } from 'react-native';
+import { useNavigation, NavigationProp, ParamListBase } from '@react-navigation/native';
+import DefaultView from '../../components/SafeAreaView';
+import Header from '../../components/Header';
+import TextBox from '../../components/TextBox';
+import styles from './index.style';
+import { WHITE_COLOR } from '../../styles/colors';
+import ColorPicker from '../../components/ColorPicker';
+import DurationPicker from '../../components/DurationPicker';
+import { CustomFontText } from '../../components/Text';
+import PrimaryButton from './components/PrimaryButton';
+import DangerButton from './components/DangerButton';
+import { ColorType } from '../../components/ColorPicker/colorblock';
+
+export default function UpdateTimerScreen(): JSX.Element {
+  // Navigation
+  const navigation = useNavigation<NavigationProp<ParamListBase>>();
+
+  // State
+  const name = useRef('');
+  const color = useRef('#F77F00');
+  const duration = useRef(0);
+
+  // Events
+  const cancel = useCallback(() => {
+    if (navigation.canGoBack()) { navigation.goBack(); }
+  }, []);
+
+  const onNameChosen = useCallback((timerName: string) => {
+    // eslint-disable-next-line no-console
+    name.current = timerName;
+  }, []);
+
+  const onColorChosen = useCallback((timerColor: ColorType) => {
+    // eslint-disable-next-line no-console
+    color.current = timerColor;
+  }, []);
+
+  const onDurationChosen = useCallback((timerDuration: number) => {
+    // eslint-disable-next-line no-console
+    duration.current = timerDuration;
+  }, []);
+
+  const onCreateChosen = useCallback(() => {
+    // After creating a timer -> Go back
+    navigation.goBack();
+  }, []);
+
+  return (
+    <DefaultView color={WHITE_COLOR}>
+      <ScrollView>
+        <View style={styles.title}>
+          <CustomFontText>
+            <Text style={styles.titleText}>Update timer</Text>
+          </CustomFontText>
+        </View>
+        <View style={styles.container}>
+          <View style={styles.spaced}>
+            <Header>Select name</Header>
+            <TextBox placeholder="Enter time" onChangeText={onNameChosen} />
+          </View>
+
+          <View style={styles.spaced}>
+            <Header>Select color</Header>
+            <ColorPicker onColorChosen={onColorChosen} />
+          </View>
+
+          <View style={styles.spaced}>
+            <Header>Select duration</Header>
+            <DurationPicker onDurationChosen={onDurationChosen} />
+          </View>
+
+          <View style={styles.buttonSpaced}>
+            <PrimaryButton title="Create timer" onPress={onCreateChosen} />
+          </View>
+
+          <View style={styles.spaced}>
+            <DangerButton title="Delete" onPress={cancel} />
+          </View>
+        </View>
+      </ScrollView>
+    </DefaultView>
+  );
+}
