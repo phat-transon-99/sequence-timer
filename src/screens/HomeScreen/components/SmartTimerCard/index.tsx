bb0c78962acf9b6dbcf998cdea05f9381c5ffc15
phat-transon-99 <118055078+phat-transon-99@users.noreply.github.com>
2022-12-30 15:02:46 +0700

    Fix bugs: Change main timer when one is running

--- src/screens/HomeScreen/components/SmartTimerCard/index.tsx
+++ src/screens/HomeScreen/components/SmartTimerCard/index.tsx
@@ -2,8 +2,9 @@ import { NavigationProp, ParamListBase, useNavigation } from '@react-navigation/
 import React, { useCallback } from 'react';
 import { UPDATE_TIMER_SCREEN } from '../../../../constants/navigation.screen';
 import { setStartDuration } from '../../../../features/RunTimer/runTimerSlice';
+import { selectHasStarted } from '../../../../features/RunTimer/selector';
 import { setMainTimer } from '../../../../features/SetMainTimer/setMainTimerSlice';
-import { useAppDispatch } from '../../../../hooks/redux';
+import { useAppDispatch, useAppSelector } from '../../../../hooks/redux';
 import Timer from '../../../../models/Timer';
 import TimerCard from '../TimerCard';
 
@@ -16,6 +17,9 @@ export default function SmartTimerCard({
   // Set main timer
   const dispatch = useAppDispatch();
 
+  // Check if the timer has started yet
+  const hasTimerStarted = useAppSelector(selectHasStarted);
+
   // On card pressed
   const onCardPressed = useCallback(() => {
     navigation.navigate(UPDATE_TIMER_SCREEN, {
@@ -25,14 +29,17 @@ export default function SmartTimerCard({
 
   // On start timer button pressed
   const onStartPressed = useCallback(() => {
-    // Dispatch set main timer
-    dispatch(setMainTimer({
-      id, name, color, duration,
-    }));
+    if (!hasTimerStarted) {
+      // Only when a timer is not started -> Run timer
+      // Dispatch set main timer
+      dispatch(setMainTimer({
+        id, name, color, duration,
+      }));
 
-    // Dispatch set timer start duration
-    dispatch(setStartDuration(duration));
-  }, [id, name, color, duration]);
+      // Dispatch set timer start duration
+      dispatch(setStartDuration(duration));
+    }
+  }, [id, name, color, duration, hasTimerStarted]);
 
   return (
     <TimerCard
--- src/screens/HomeScreen/index.tsx
+++ src/screens/HomeScreen/index.tsx
@@ -1,24 +1,41 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 import { View } from 'react-native';
+import BottomSheet from '@gorhom/bottom-sheet';
 import TimerCardList from './components/SmartTimerList';
 import styles from './index.style';
 import CustomBottomSheet from '../../components/CustomBottomSheet';
 import SmartHeader from './components/SmartHeader';
-import { useAppDispatch } from '../../hooks/redux';
+import { useAppDispatch, useAppSelector } from '../../hooks/redux';
 import { fetchAllTimers } from '../../features/MangeTimer/thunk';
 import SmartTimerSection from './components/SmartTimerSection';
+import { selectHasStarted } from '../../features/RunTimer/selector';
 
 function HomeScreen(): JSX.Element {
+  // Control bottom sheet
+  const bottomSheetRef = useRef<BottomSheet>(null);
+
+  // Dispatch to fetch timers
   const dispatch = useAppDispatch();
 
+  // Get the state of timer
+  const hasTimerStarted = useAppSelector(selectHasStarted);
+
+  // Fetch all timers on start
   useEffect(() => {
     dispatch(fetchAllTimers());
   }, []);
 
+  // Close the bottom sheet when timer is started
+  useEffect(() => {
+    if (hasTimerStarted) {
+      bottomSheetRef.current?.collapse();
+    }
+  }, [hasTimerStarted]);
+
   return (
     <View style={styles.screen}>
       <SmartTimerSection />
-      <CustomBottomSheet>
+      <CustomBottomSheet ref={bottomSheetRef}>
         <SmartHeader />
         <TimerCardList />
       </CustomBottomSheet>
